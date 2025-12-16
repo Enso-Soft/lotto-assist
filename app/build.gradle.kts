7e034893479cfd21c14ecc30f756ec730d2eba8e
kukwonho <kukwonho@gmail.com>
2025-12-16 10:33:24 +0900

    Build-Logic 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,39 +1,15 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.hilt)
+    id("lotto.android.application")
+    id("lotto.android.hilt")
 }
 
 android {
     namespace = "com.enso.lotto_assist"
-    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.enso.lotto_assist"
-        minSdk = 24
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
 }
 
@@ -47,15 +23,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    // Hilt 사용을 위한 의존성 추가.
-    kapt(libs.kotlinx.metadata.jvm)
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
 }
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,42 +1,14 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.hilt)
-    id("kotlinx-serialization")
+    id("lotto.android.library")
+    id("lotto.android.hilt")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.enso.network"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -51,9 +23,4 @@ dependencies {
     implementation(libs.logging.interceptor)
     implementation(libs.okhttp3.okhttp)
     implementation(libs.retrofit.kotlin.serialization)
-
-    // Hilt 사용을 위한 의존성 추가.
-    kapt(libs.kotlinx.metadata.jvm)
-    implementation(libs.hilt.android)
-    kapt("com.google.dagger:hilt-android-compiler:2.52")
 }
