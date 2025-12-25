e12c845f0bb63074ea7ff900eb2d85151fbb7628
enso <kukwonho@gmail.com>
2025-12-25 12:16:08 +0900

    레퍼런스 추가

--- feature/home/src/main/java/com/enso/home/LottoResultScreen.kt
+++ feature/home/src/main/java/com/enso/home/LottoResultScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -155,6 +156,7 @@ fun LottoResultScreen(
         },
         snackbarHost = { SnackbarHost(snackbarHostState) },
         containerColor = lottoColors.backgroundLight,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         modifier = modifier
     ) { paddingValues ->
         Column(
--- feature/my-lotto/src/main/java/com/enso/mylotto/MyLottoScreen.kt
+++ feature/my-lotto/src/main/java/com/enso/mylotto/MyLottoScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -86,7 +87,8 @@ fun MyLottoScreen(
             )
         },
         snackbarHost = { SnackbarHost(snackbarHostState) },
-        containerColor = lottoColors.backgroundLight
+        containerColor = lottoColors.backgroundLight,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
     ) { paddingValues ->
         PullToRefreshBox(
             isRefreshing = uiState.isRefreshing,
